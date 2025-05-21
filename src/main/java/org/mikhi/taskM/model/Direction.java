67f453bda5c02d3af2b0a5f8325529fc7c670ec4
mikhi-mh <hmikhilesh@gmail.com>
2025-05-21 19:05:04 +0530

    impleted review comments/removed custom converter

--- src/main/java/org/mikhi/taskM/model/Direction.java
+++ src/main/java/org/mikhi/taskM/model/Direction.java
@@ -2,16 +2,4 @@ package org.mikhi.taskM.model;
 
 public enum Direction {
   ASC, DESC;
-
-  public static Direction fromString(String value) {
-    if (value == null) {
-      throw new IllegalArgumentException("Sort direction cannot be null");
-    }
-    try {
-      return Direction.valueOf(value.toUpperCase());
-    } catch (IllegalArgumentException ex) {
-      throw new IllegalArgumentException(
-          "Invalid sort direction: " + value + ". Allowed values are: asc, desc");
-    }
-  }
 }
