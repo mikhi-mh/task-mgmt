d783138fa1494e994287ade699ce5d84f0169322
mikhi-mh <hmikhilesh@gmail.com>
2025-05-24 00:16:49 +0530

    junits for controller-methods

--- src/main/java/org/mikhi/taskM/model/Task.java
+++ src/main/java/org/mikhi/taskM/model/Task.java
@@ -8,10 +8,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
 
   @Id
